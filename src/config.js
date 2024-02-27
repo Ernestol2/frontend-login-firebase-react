48078d659cd06b05ec34af2ca10f3c0cb3c09849
user.name: "Ernesto Lopez <user.email: "chefernestolopez@gmail.com>
2024-02-27 10:55:11 -0300

    protecting firebase credentials

--- src/config.js
+++ src/config.js
@@ -1,14 +1,7 @@
 import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
+import  firebaseConfig from "../firebaseConfig";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyDtHY9DgWh7mFL1CX_jZWfMXtw1mOW_M34",
-  authDomain: "login-test-30b9a.firebaseapp.com",
-  projectId: "login-test-30b9a",
-  storageBucket: "login-test-30b9a.appspot.com",
-  messagingSenderId: "365350398956",
-  appId: "1:365350398956:web:8654f84d1ac76fc9c86ec9"
-};
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
